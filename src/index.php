52de0904e54bf61b489d13b58565ca31679b64d9
ehlzz <hlzemile@gmail.com>
2024-11-02 20:11:48 +0100

    horrible

---
+++ src/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>PHP Test</title>
+    <link rel="stylesheet" href="../public/css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="login.php">Logi</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
+    </main>
+</body>
+</html>
